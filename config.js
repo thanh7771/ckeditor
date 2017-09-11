e1fc0e73b9377964eb36b36bb1e7f0432187b7df
thanh7771 <thanh7771@gmail.com>
2017-09-11 13:49:29 +0700

    Commit code

---
+++ config.js
@@ -0,0 +1,12 @@
+﻿/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+
+	// %REMOVE_START%
+	// The configuration options below are needed when running CKEditor from source files.
+	config.plugins = 'dialogui,dialog,about,a11yhelp,dialogadvtab,basicstyles,bidi,blockquote,notification,button,toolbar,clipboard,panelbutton,panel,floatpanel,colorbutton,colordialog,templates,menu,contextmenu,div,enterkey,entities,popup,filebrowser,find,fakeobjects,flash,htmlwriter,wysiwygarea,table,tabletools,tableselection,menubutton,language,link,liststyle,indent,indentlist,list,newpage,pagebreak,maximize,preview,pastefromword,removeformat,listblock,richcombo,zoom,magicline,pastetext,save,selectall,showblocks,showborders,sourcearea,print,specialchar,justify,smiley,indentblock,iframe,horizontalrule,format,forms,font,floatingspace,resize,copyformatting,elementspath,image,lineutils,widgetselection,widget,image2,imageresponsive,letterspacing,undo,stylescombo,tab,scayt';
+	config.skin = 'bootstrapck';
+};
